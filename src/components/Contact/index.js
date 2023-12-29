f31ba58f71e9bfdc27880601cb2f67a21bd6da6d
ahmedsalman74 <saprysalman586@gmail.com>
2023-12-29 17:37:09 +0200

    setup Email js fun

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_0guun27', 'template_2gz4pu8', form.current, 'ab1YrSwGV-XHdHwND')
       .then((result) => {
         setOpen(true);
         form.current.reset();
