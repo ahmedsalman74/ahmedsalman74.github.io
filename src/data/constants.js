4d9c8962d71209a5b5a5ca977b435e482921dc5f
ahmedsalman74 <saprysalman586@gmail.com>
2023-12-30 18:29:41 +0200

    cv link updte

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/ahmedsalman74",
   resume:
-    "https://drive.google.com/file/d/1ThCXz4XQY8JhywQ25ikFd8Lh8ch2pIFU/view?usp=sharing",
+    "https://drive.google.com/drive/folders/17deURwWc9EduBu2cieodqCIMOCq6kK4n?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ahmedsalman74/",
   twitter: "https://twitter.com/Ahmedsalman47_",
   insta: "",
